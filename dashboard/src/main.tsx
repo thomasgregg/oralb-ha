3a16da5987bf931f695b08ee1fe85fa7090cad87
Thomas Gregg <thomas@gregg.de>
2026-08-21 23:12:30 +0200

    Bundle dashboard fonts

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import '@fontsource-variable/jetbrains-mono';
+import '@fontsource-variable/manrope';
 import App from './App';
 import './styles.css';
 
